c5f64e5237b0ced3df5fd1dc7615f24d3c52d611
Brian Acker <backer@brightcove.com>
2017-05-23 16:42:23 +0900

    Add waitForFile function to make dump_heap script work

--- dump_heap.sh
+++ dump_heap.sh
@@ -2,14 +2,37 @@
 
 #script to take a heap dump of running application via command line
 
-#NOTE this currently has problems where adb pull grabs an empty file
-
 #PROCESS is required
 PROCESS=""
 FILENAME="heapdump-$(date +"%m_%d_%Y_%H%M")"
 CONVERT=false
 #required flag p: sets the process to dump heap from
 #optional flag c indicates that the file should be converted to standard hprof
+
+fileSize() {
+	local size=$(echo $(adb shell "ls -s /data/local/tmp/${FILENAME}-unconverted.hprof") | cut -f1 -d' ')
+	echo $size
+}
+
+waitForFile() {
+	# wait for file to stabalize at a size above 0
+	local lastSize=0
+	local matchCount=0
+	while [ ${matchCount} -lt 3 ] ;
+	do
+		if [[ ${lastSize} -gt 0 && $(fileSize) = ${lastSize} ]] ;
+			then
+			let "matchCount+=1"
+			echo "match ${matchCount}"
+		else
+			matchCount=0
+			lastSize=$(fileSize)
+			echo "Dumping file...current size = ${lastSize}"
+		fi
+		sleep 0.5
+	done
+	echo "Heap Dump Complete, file size = ${lastSize}"
+}
 
 while getopts 'p:c' opt;
 do
@@ -31,6 +54,7 @@ fi
 
 echo "dumping heap of ${PROCESS} to file: ${FILENAME}"
 adb shell "am dumpheap ${PROCESS} /data/local/tmp/${FILENAME}-unconverted.hprof"
+waitForFile
 adb pull "/data/local/tmp/${FILENAME}-unconverted.hprof"
 adb shell "rm /data/local/tmp/${FILENAME}-unconverted.hprof"
 
